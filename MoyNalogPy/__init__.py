d5396cf72d255328a440b94aacc3f2e1382ab2e2
Arseniy <senylar803@gmail.com>
2025-08-21 10:56:27 +0300

    fix

--- MoyNalogPy/__init__.py
+++ MoyNalogPy/__init__.py
@@ -1,4 +1,5 @@
 from .MoyNalog import MoyNalog
 from .schemas.schemas import Service, Client, ProfileStorage, Incomes, Receipt, UserProfile
+from .auth import Authentication
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
