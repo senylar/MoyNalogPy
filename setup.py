e8ff219d6fef595cd2d3ab05f88e1662b170f666
Arseniy <senylar803@gmail.com>
2025-08-26 20:45:43 +0300

    upd vers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="moy-nalog-py",
-    version="0.2.0",
+    version="0.2.1",
     author="senylar",
     author_email="senyvlar@gmail.com",
     description="Python клиент для работы с API сервиса Мой налог",
